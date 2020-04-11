fb70df4f67b0209c8ef4c6c6f123bf8aa58ba165
Fabian Schebera <fabian.schebera@diovision.net>
2020-04-12 00:00:15 +0200

    Added StreakData

--- src/@types/streak.d.ts
+++ src/@types/streak.d.ts
@@ -1,3 +1,39 @@
 export declare interface StreakData {
-
+    supportScorestreakData: SupportStreakData;
+    lethalScorestreakData: LethalStreakData;
+}
+declare type LethalStreakData {
+    [key: Lethal]: StreakProperties;
+}
+declare type SupportStreakData {
+    [key: Support]: StreakProperties;
+}
+declare interface StreakProperties {
+    awardedCount: number;
+    extraStat1: number;
+    uses: number;
+}
+declare const enum Support {
+    AIRDROP = 'airdrop',
+    EMERGENCY_AIRDROP = 'airdrop_multiple',
+    UAV = 'uav',
+    COUNTER_UAV = 'scrambler_drone_overwatch',
+    PERSONAL_RADAR = 'radar_drone_overwatch',
+    ORBITAL_UAV = 'directional_uav'
+}
+declare const enum Lethal {
+    WHITE_PHOSPHORUS = 'white_phosphorus',
+    CLUSTER_STRIKE = 'toma_strike',
+    SENTRY_GUN = 'sentry_gun',
+    PRECISION_AIRSTRIKE = 'precision_airstrike',
+    NUKE = 'nuke',
+    SHIELD_TURRET = 'manual_turret',
+    JUGGERNAUT = 'juggernaut',
+    VTOL = 'hover_jet',
+    GUNSHIP = 'gunship',
+    CRUISE_MISSILE = 'cruise_predator',
+    SUPPORT_HELO = 'chopper_support',
+    CHOPPER_GUNNER = 'chopper_gunner',
+    WHEELSON = 'pac_sentry',
+    TANK = 'bradley'
 }
