5ba5d0a2f93e0a28e656240f1afd6029991a22bd
realdegrees <fabian.schebera@gmail.com>
2020-04-12 17:01:36 +0200

    Refactors d.ts files for easier access/typings

--- src/@types/mode.d.ts
+++ src/@types/mode.d.ts
@@ -3,29 +3,89 @@ export type ModeData = {
 };
 
 declare type ModeProperties = {
-    [key in ModeStat]: number;
+    [key in BaseProperty]: number;
 }
-declare type ModeStat =
+
+export namespace Mode {
+    export const enum Warzone {
+        BATTLE_ROYALE = 'br',
+        PLUNDER = 'br_dmz',
+        ALL = 'br_all'
+    }
+    export namespace Multiplayer {
+        export const enum Hardcore {
+            TEAM_DEATHMATCH = 'hc_war',
+            HARDPOINT = 'hc_koth',
+            HEADQUARTERS = 'hc_hq',
+            SEARCH_AND_DESTROY = 'hc_sd',
+            CYBER_ATTACK = 'hc_cyber',
+            KILL_CONFIRMED = 'hc_conf',
+            DOMINATION = 'hc_dom'
+        }
+        export const enum Core {
+            TEAM_DEATHMATCH = 'war',
+            HARDPOINT = 'koth',
+            HEADQUARTERS = 'hq',
+            SEARCH_AND_DESTROY = 'sd',
+            CYBER_ATTACK = 'cyber',
+            KILL_CONFIRMED = 'conf',
+            DOMINATION = 'dom'
+        }
+    }
+}
+export declare type Mode =
+    Mode.Warzone |
+    Mode.Multiplayer.Core |
+    Mode.Multiplayer.Hardcore;
+
+export declare type BaseProperties = {
+    [key in BaseProperty]: number;
+}
+export declare type TeamDeathMatchProperties = BaseProperties & {
+    [key in TeamDeathMatchProperty]: number;
+}
+export declare type HardpointProperties = BaseProperties & {
+    [key in HardpointProperty]: number;
+}
+export declare type HeadquartersProperties = BaseProperties & {
+    [key in HeadquarterProperty]: number;
+}
+export declare type SearchAndDestroyProperties = BaseProperties & {
+    [key in SearchAndDestroyProperty]: number;
+}
+export declare type CyberAttackProperties = BaseProperties & {
+    [key in CyberAttackProperty]: number;
+}
+export declare type KillConfirmedProperties = BaseProperties & {
+    [key in KillConfirmedProperty]: number;
+}
+export declare type DominationProperties = BaseProperties & {
+    [key in DominationProperty]: number;
+}
+declare type BaseProperty =
     'kills' |
-    'score' |
-    'timePlayed' |
+    'deaths' |
     'kdRatio' |
-    'assists' |
+    'score' |
     'scorePerMinute' |
-    'deaths';
-declare type Mode =
-    'dom' |
-    'war' |
-    'hq' |
-    'hc_dom' |
-    'hc_conf' |
-    'koth' |
-    'conf' |
-    'hc_hq' |
-    'br_dmz' |
-    'sd' |
-    'cyber' |
-    'hc_war' |
-    'br_all' |
-    'hc_sd' |
-    'hc_cyber';
+    'timePlayed';
+declare type KillConfirmedProperty =
+    'confirms' |
+    'denies';
+declare type TeamDeathMatchProperty =
+    'assists';
+declare type HardpointProperty =
+    'defends' |
+    'objTime';
+declare type HeadquarterProperty =
+    'captures' |
+    'defends';
+declare type SearchAndDestroyProperty =
+    'defuses' |
+    'plants';
+declare type CyberAttackProperty =
+    'plants' |
+    'revives';
+declare type DominationProperty =
+    'captures' |
+    'defends';
--- src/@types/streak.d.ts
+++ src/@types/streak.d.ts
@@ -1,14 +1,9 @@
 export declare interface StreakData {
-    supportScorestreakData: SupportStreakData;
-    lethalScorestreakData: LethalStreakData;
+    supportScorestreakData: Streaks<Streak.Support>;
+    lethalScorestreakData: Streaks<Streak.Lethal>;
 }
-declare type LethalStreakData = {
-    [key in Lethal]: {
-        properties: StreakProperties
-    };
-}
-declare type SupportStreakData = {
-    [key in Support]: {
+declare type Streaks<T extends Streak> = {
+    [key in T]: {
         properties: StreakProperties
     };
 }
@@ -17,27 +12,30 @@ declare interface StreakProperties {
     extraStat1: number;
     uses: number;
 }
-declare const enum Support {
-    AIRDROP = 'airdrop',
-    EMERGENCY_AIRDROP = 'airdrop_multiple',
-    UAV = 'uav',
-    COUNTER_UAV = 'scrambler_drone_overwatch',
-    PERSONAL_RADAR = 'radar_drone_overwatch',
-    ORBITAL_UAV = 'directional_uav'
-}
-declare const enum Lethal {
-    WHITE_PHOSPHORUS = 'white_phosphorus',
-    CLUSTER_STRIKE = 'toma_strike',
-    SENTRY_GUN = 'sentry_gun',
-    PRECISION_AIRSTRIKE = 'precision_airstrike',
-    NUKE = 'nuke',
-    SHIELD_TURRET = 'manual_turret',
-    JUGGERNAUT = 'juggernaut',
-    VTOL = 'hover_jet',
-    GUNSHIP = 'gunship',
-    CRUISE_MISSILE = 'cruise_predator',
-    SUPPORT_HELO = 'chopper_support',
-    CHOPPER_GUNNER = 'chopper_gunner',
-    WHEELSON = 'pac_sentry',
-    TANK = 'bradley'
+declare type Streak = Streak.Support | Streak.Lethal;
+export namespace Streak {
+    const enum Support {
+        AIRDROP = 'airdrop',
+        EMERGENCY_AIRDROP = 'airdrop_multiple',
+        UAV = 'uav',
+        COUNTER_UAV = 'scrambler_drone_overwatch',
+        PERSONAL_RADAR = 'radar_drone_overwatch',
+        ORBITAL_UAV = 'directional_uav'
+    }
+    const enum Lethal {
+        WHITE_PHOSPHORUS = 'white_phosphorus',
+        CLUSTER_STRIKE = 'toma_strike',
+        SENTRY_GUN = 'sentry_gun',
+        PRECISION_AIRSTRIKE = 'precision_airstrike',
+        NUKE = 'nuke',
+        SHIELD_TURRET = 'manual_turret',
+        JUGGERNAUT = 'juggernaut',
+        VTOL = 'hover_jet',
+        GUNSHIP = 'gunship',
+        CRUISE_MISSILE = 'cruise_predator',
+        SUPPORT_HELO = 'chopper_support',
+        CHOPPER_GUNNER = 'chopper_gunner',
+        WHEELSON = 'pac_sentry',
+        TANK = 'bradley'
+    }
 }
